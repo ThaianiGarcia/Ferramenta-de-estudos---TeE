929a02ca6b39e99768bf2e2cf80adf4162744246
ThaianiGarcia <thaiani.garcia@escola.pr.gov.br>
2025-09-30 22:00:45 +0000

    Terminação parcial2

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function criaCartao(categoria, pergunta, resposta) {
-        let container = document.getElementById('container')
-        let cartao = document.createElement('article')
-        cartao.className = 'cartao'
+function criaCartao (categoria, pergunta, resposta){
+    let container = document.getElementById('container')
+    let cartao =document.createElement('article')
+    cartao.className ='cartao'
 
-        cartao.innerHTML = `
+cartao.innerHTML = `
         <div class="cartao__conteudo">
         <h3>${categoria}</h3>
         <div class="cartao__conteudo__pergunta">
@@ -12,17 +12,7 @@ function criaCartao(categoria, pergunta, resposta) {
         <div class="cartao__conteudo__resposta">
                 <p>${resposta}</p>
         </div>
-        </div> 
+        </div>
 `
-
-let respostaEstaVisivel = false
-
-function viraCartao(){
-    respostaEstaVisivel = !respostaEstaVisivel
-    cartao.classList.toggle('active', respostaEstaVisivel)
-}
-
-cartao.addEventListener('click', viraCartao)
-
 container.appendChild(cartao)
 }
--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,53 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'programação',
+    ' O que é a tag footer',
+    ' É a tag utlizada para criar o rodapé'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'programação',
+    'O que é CSS?',
+    'É uma linguagem de programação utilizada para dar estilo a página'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'programação',
+    ' O que é javaScript',
+    'O JavaScript (JS) é uma libguagem de programação que nos permite interagir com a página'
 )
 
-    criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
-) 
+criaCartao(
+    'programação',
+    ' O que é html',
+    ' É uma linguagem de programação que nos permite criar a estrutura do noso site.'
+)
+
+criaCartao(
+    'programação',
+    ' Para que serve a tag article',
+    ' Para criar um conteudo ou sistema independent. No nosso caso estamos usando para criar cartões de estudo.'
+)
+
+criaCartao(
+    'programação',
+    ' Qual a diferença entre a tag title que usamos na tag h1 que usamos na tag body',
+    ' A tag title aparecera no site da página que será aberta e a tag do titulo h1 é usada no corpodo site como um titulo principal da página. '
+)
+
+criaCartao(
+    'programação',
+    'Qual a diferença entre tags head e header',
+    ' head é tag de configurações, tudo que fizemos dentro dela, não aparecerá na página do nosso site. Header é tah de cabeçalho da página.'
+)
+
+criaCartao(
+    'programação',
+    ' Pra que serve o :root  utilizando no CSS?',
+    ' Serve para alvar variaveis para serem utilizadas durante o desenvolvimento da página, como por exemplo: cores e fontes.'
+)
+
+criaCartao(
+    'programação',
+    ' Qual a diferença entre margin e padding',
+    'Margin é o espaço externo enquanto o padding é espaçamento interno.'
+)
